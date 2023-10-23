e56530de73d9483d7f23f081e6080c5295db5109
Zoja7 <yaozonka@gmail.com>
2023-10-23 14:29:03 +0300

    commit_13

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
 import css from './Notification.module.css';
 export default function Notification({ message = '' }) {
-  return <p>{message}</p>;
+  return <p className={css.message}>{message}</p>;
 }
